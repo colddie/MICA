66ade718930b01f726d611918f70841cc44d2de8
Julio <colddiesun@gmail.com>
2013-06-02 21:07:44 +0800

    Add crosshairs

--- Qt/PanelViewer/src/panelViewer.h
+++ Qt/PanelViewer/src/panelViewer.h
@@ -16,18 +16,33 @@
 #define _PanelViewer_H
 
 #include "vtkSmartPointer.h"    // Required for smart pointer internal ivars.
+#include "vtkInteractorStyleTrackballCamera.h"
+#include "vtkInteractorStyleImage.h"
+#include "vtkLineSource.h"
+#include "vtkActor.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+
 #include <QMainWindow>
 
 
-
-// Forward Qt class declarations
+// Forward vtk Qt class declarations
 class vtkImageViewer2;
 class vtkRenderer;
+class vtkObject;
+class vtkCommand;
+class vtkEventQtSlotConnect;
+class vtkRenderWindowInteractor;
+class vtkInteractorStyleTrackballCamera;
+class vtkInteractorStyleImage;
 
+
+// Add mainwindow to ui struct
 namespace Ui {
 class panelViewer;
 }
 
+//
 class Viewer : public QMainWindow
 {
   Q_OBJECT
@@ -54,11 +69,26 @@ public slots:
 //    virtual void SetSlice_3(int slice);
     virtual void SetWindowLevel(int index);
 
+//    virtual void vtkEventCallBack(vtkObject *caller, unsigned long vtkEvent,
+//                                  void *clientData, void *callData, vtkCommand *command);
+
+    void mouseEnterCallback_1();
+    void mouseEnterCallback_2();
+    void mouseEnterCallback_3();
+    void mouseLeaveCallback();
+
+    void mouseWheelForwardCallback(vtkObject*,ulong,void*,void*,vtkCommand*);
+    void mouseWheelBackwardCallback(vtkObject*,ulong,void*,void*,vtkCommand*);
+    void mouseMoveCallback(vtkObject*,ulong,void*,void*,vtkCommand*);
+    void mouseClickCallback(vtkObject*,ulong,void*,void*,vtkCommand*);
+
+
 protected:
 
     void LoadVolume(std::string filename);
     void UpdateWidget();
-
+    void AddCrosshair();
+    void RedrawCrosshair();
 
 protected slots:
 
@@ -68,17 +98,84 @@ private:
                                     viewerCoronal,
                                     viewerSagittal;
     //    string m_filename;
-    vtkSmartPointer<vtkRenderer> render1;
-    vtkSmartPointer<vtkRenderer> render2;
-    vtkSmartPointer<vtkRenderer> render3;
+    //    vtkSmartPointer<vtkRenderer> render1;
+    //    vtkSmartPointer<vtkRenderer> render2;
+    //    vtkSmartPointer<vtkRenderer> render3;
+
+    // must declare as class member instead define in the function
+    vtkSmartPointer<vtkEventQtSlotConnect> connection;
 
     // Designer form
     Ui::panelViewer *ui;
 
+    //
+    QList<unsigned int> imageDimension;
+    QList<double> imageSpacing;
+    QList<int> currentPosition;
+    QString currentWidget;
+    QList<vtkSmartPointer<vtkLineSource> > horizontalLine;
+    QList<vtkSmartPointer<vtkLineSource> > verticalLine;
+    QList<vtkSmartPointer<vtkPolyDataMapper> > horizontalLineMapper;
+    QList<vtkSmartPointer<vtkPolyDataMapper> > verticalLineMapper;
+    QList<vtkSmartPointer<vtkActor> > horizontalLineActor;
+    QList<vtkSmartPointer<vtkActor> > verticalLineActor;
+
+    //
+    bool loadImage;
+    //    bool mouseOverWidget;
 
 };
 
 
 
+// Crosshair implementation
+//class Crosshair
+//{
+//};
+
+
+// Overwrite default interaction style
+class MouseInteractorStyle : public vtkInteractorStyleImage // Or vtkInteractorImage
+{
+  public:
+    static MouseInteractorStyle* New();
+    vtkTypeMacro(MouseInteractorStyle, vtkInteractorStyleImage);
+
+    virtual void OnLeftButtonDown()
+    {
+      std::cout << "Pressed left mouse button." << std::endl;
+      // Forward events
+      // vtkInteractorStyleTrackballCamera::OnLeftButtonDown();
+    }
+
+//    virtual void OnMiddleButtonDown()
+//    {
+//      std::cout << "Pressed middle mouse button." << std::endl;
+//      // Forward events
+//      // vtkInteractorStyleTrackballCamera::OnMiddleButtonDown();
+//    }
+
+//    virtual void OnRightButtonDown()
+//    {
+//      std::cout << "Pressed right mouse button." << std::endl;
+//      // Forward events
+//      // vtkInteractorStyleTrackballCamera::OnRightButtonDown();
+//    }
+
+    virtual void OnMouseWheelForward()
+    {
+        std::cout << "Forward scroll mouse wheel." << std::endl;
+        // Forward events
+        // vtkInteractorStyleTrackballCamera::OnMouseWheelForward();
+    }
+
+    virtual void OnMouseWheelBackward()
+    {
+        std::cout << "Forward scroll mouse wheel." << std::endl;
+        // Forward events
+        // vtkInteractorStyleTrackballCamera::OnMouseWheelBackward();
+    }
+
+};
 
 #endif // SimpleView_H
