31355557921f968956790d23cd678976588fb7a2
Julio <colddiesun@gmail.com>
2013-06-05 21:13:54 +0800

    PanelViewer: upload new source

---
+++ Qt/PanelViewer/src/main.cxx
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2013 Julio Sun.
+ * Redistribution and use in source and binary forms, with
+ * or without modification, are permitted provided that this Notice and any
+ * statement of authorship are reproduced on all copies.
+ */
+
+// QT includes
+#include <QApplication>
+//#include <QCleanlooksStyle>
+
+#include "panelViewer.h"
+//#include "argumentsParse.h"
+
+extern int qInitResources_icons();
+
+
+int main( int argc, char** argv )
+{
+
+
+    // QT Stuff
+    QApplication app( argc, argv );
+
+    //QApplication::setStyle(new QCleanlooksStyle);
+
+    qInitResources_icons();
+
+//    Viewer mySimpleView(windowhigh, windowlow, filename);
+    Viewer mySimpleView;
+//    mySimpleView.Run();
+    mySimpleView.show();
+
+    return app.exec();
+}
